61af71f7f09e073bd82a601c9a95333359411c54
henrry <henrrybrgt@gmail.com>
2023-09-10 21:22:49 -0400

    finished for now version module

--- src/version/dto/version.dto.ts
+++ src/version/dto/version.dto.ts
@@ -1,16 +1,27 @@
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+
 // Create
 export class CreateVersionDto {
+  id: number = 0;
+  @ApiProperty()
   date: Date;
+  @ApiProperty()
   description: string;
+  @ApiProperty()
   tag: string;
+  @ApiProperty()
   documentId: number;
 }
 
 // Update
 export class UpdateVersionDto {
+  @ApiPropertyOptional()
   date?: Date;
+  @ApiPropertyOptional()
   description?: string;
+  @ApiPropertyOptional()
   tag?: string;
+  @ApiPropertyOptional()
   documentId?: number;
 }
 
--- src/version/version.controller.ts
+++ src/version/version.controller.ts
@@ -1,16 +1,62 @@
 import {
   Controller,
-  // Get,
-  // Post,
-  // Body,
-  // Patch,
-  // Param,
-  // Delete,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
 } from '@nestjs/common';
 import { VersionService } from './version.service';
-// import { CreateVersionDto, UpdateVersionDto } from './dto/version.dto';
+import { CreateVersionDto, UpdateVersionDto } from './dto/version.dto';
+import { Version } from './entity/version.entity';
+import { ApiBadRequestResponse, ApiNotFoundResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { DeleteResult } from 'typeorm';
 
+@ApiTags('version')
 @Controller('version')
 export class VersionController {
   constructor(private readonly versionService: VersionService) {}
+
+  @ApiOkResponse({ type: Version })
+  @Post()
+  async createVersion(@Body() version: CreateVersionDto): Promise<Version> {
+    return await this.versionService.create({ ...version });
+  }
+
+  @ApiOkResponse({ type: Version })
+  @ApiBadRequestResponse({ type: undefined, description: 'Bad Request' })
+  @ApiNotFoundResponse({ type: undefined, description: 'Version Not Found' })
+  @Get(':id')
+  async getVersion(@Param('id') stringId: string): Promise<Version | null> {
+    const id = Number(+stringId);
+    return await this.versionService.getOne(id);
+  }
+
+  @ApiOkResponse({ type: Version, isArray: true })
+  @Get()
+  async getVersions(): Promise<Version[]> {
+    return await this.versionService.getAll();
+  }
+
+  @ApiOkResponse({ type: Version })
+  @Patch(':id')
+  async updateVersion(
+    @Param('id') stringId: string,
+    @Body() updatedVersion: UpdateVersionDto,
+  ): Promise<Version> {
+    const id = Number(+stringId);
+    const version = (await this.versionService.getOne(id)) as Version;
+    return await this.versionService.update(
+      { ...version },
+      { ...updatedVersion },
+    );
+  }
+
+  @ApiOkResponse({ type: Version })
+  @Delete(':id')
+  async deleteVersion(@Param('id') stringId: string): Promise<DeleteResult> {
+    const id = Number(+stringId);
+    return await this.versionService.del({ id });
+  }
 }
