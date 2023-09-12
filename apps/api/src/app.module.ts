bb48d8fd89d6caa5a99606d628d1ff9208245195
henrry <henrrybrgt@gmail.com>
2023-09-11 21:59:33 -0400

    changed to monorepo

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,42 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { DocumentModule } from './document/document.module';
+import { EmployeeProjectModule } from './employee-project/employee-project.module';
+import { EmployeeModule } from './employee/employee.module';
+import { ProjectModule } from './project/project.module';
+import { TaskModule } from './task/task.module';
+import { VersionModule } from './version/version.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join, resolve } from 'path';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ expandVariables: true, isGlobal: true, envFilePath: resolve(__dirname, '../.env') }),
+    EmployeeModule,
+    ProjectModule,
+    TaskModule,
+    DocumentModule,
+    VersionModule,
+    AuthModule,
+    EmployeeProjectModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get("DBHOST") ?? "",
+        port: parseInt(configService.get("DBPORT") as string),
+        username: configService.get("DBUSER"),
+        password: configService.get("DBPASS"),
+        database: configService.get("DBNAME"),
+        entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+        synchronize: false,
+      }),
+      inject: [ConfigService]
+    })
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,10 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as dotenv from 'dotenv'
+import { NextFunction } from 'express';
+
+dotenv.config({ path: join(__dirname, '../.env') });
+
+async function importHandler() {
+  const module = await import(join(__dirname,'../../web/build/handler.js'));
+  return module;
+}
 
 async function bootstrap() {
   const startTime = process.hrtime();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const logger = new Logger();
 
   const config = new DocumentBuilder()
@@ -21,9 +32,21 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  SwaggerModule.setup('api', app, document);
 
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  SwaggerModule.setup('api/docs', app, document);
+  app.setGlobalPrefix('api');
+
+  if (process.env.NODE_ENV === 'production') {
+    const handler = (await importHandler()).handler;
+
+    app.use((req: Request, res: Response, next: NextFunction) => {
+      if (req.url.startsWith("/api")) {
+        return next();
+      }
+      return handler(req, res, next);
+    });
+  }
 
   await app.listen(3000);
 
