f75205a4c4b090cdca97159b4ddcef28a87b007a
henrry <henrrybrgt@gmail.com>
2023-09-13 08:20:52 -0400

    added +error.svelte

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -25,30 +25,7 @@
 				<a href="/"><img src="{titleImg}" alt="HenTech Logo" class="w-1/3"></a>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+				<a href="/login" class="btn btn-sm variant-ghost-surface">Login</a>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -57,7 +57,11 @@
   </div>
 </section>
 <section class="text-gray-600 body-font relative">
-  <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+  <div class="lg:w-1/2 w-full lg:mb-0 lg:mx-28 md:mx-14 mx-7 text-white">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2">Contact us</h1>
+      <div class="h-1 w-20 bg-indigo-500 rounded"></div>
+    </div>
+  <div class="container px-5 pt-5 pb-24 mx-auto flex sm:flex-nowrap flex-wrap">
     <div class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 card flex items-end justify-start relative">
       <iframe width="100%" height="100%" class="absolute inset-0" frameborder="0" title="map" marginheight="0" marginwidth="0" scrolling="no" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" style="filter: grayscale(1) contrast(1.2) opacity(0.4);"></iframe>
       <div class="bg-white relative flex flex-wrap py-6 rounded shadow-md">
