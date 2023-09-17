f5875f118de1ecf312723ab6de5f73309b9b63ee
henrry <henrrybrgt@gmail.com>
2023-09-17 01:17:17 -0400

    adding table

--- apps/web/src/routes/internal/projects/search/+page.svelte
+++ apps/web/src/routes/internal/projects/search/+page.svelte
@@ -1,5 +1,59 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
+	import { Paginator } from '@skeletonlabs/skeleton';
+	import type { PaginationSettings } from '@skeletonlabs/skeleton';
+	import type { PageData } from './$types';
+	import Icon from '@iconify/svelte';
+	export let data: PageData;
+
+	let paginationSettings: PaginationSettings = {
+		page: 0,
+		limit: 10,
+		size: data.projects.length,
+		amounts: [1, 2, 5, 10, 20]
+	};
+
+	let headers = ['ID', 'Name', 'Comercial Designation', 'Leader', 'Status', 'Actions'];
+
+	let paginatedSource: {
+		id: number;
+		status: string;
+		name: string;
+		comercialDesignation: string;
+		leader: string;
+	}[] = [];
+
+	$: paginatedSource = data.projects.slice(
+		paginationSettings.page * paginationSettings.limit,
+		paginationSettings.page * paginationSettings.limit + paginationSettings.limit
+	);
 </script>
+
+<main class="w-full">
+	<h1 class="text-3xl font-bold text-center">Search Projects</h1>
+	<table class="table !my-4">
+        <tr>
+            {#each headers as header}
+                <th class="text-left !p-3">{header}</th>
+            {/each}
+        </tr>
+        {#each paginatedSource as item}
+            <tr>
+                <td class="!p-3">{item.id}</td>
+                <td class="!p-3">{item.name}</td>
+                <td class="!p-3">{item.comercialDesignation}</td>
+                <td class="!p-3">{item.leader}</td>
+                <td class="!p-3">{item.status}</td>
+                <td class="!p-3">
+                    <a href="/internal/projects/search" class="btn btn-icon variant-ghost-primary">
+                        <Icon icon="mdi:eye-outline" class="h-"/>
+                    </a>
+                </td>
+            </tr>
+        {/each}
+    </table>
+	<Paginator
+		bind:settings={paginationSettings}
+		showFirstLastButtons={false}
+		showPreviousNextButtons={true}
+	/>
+</main>
