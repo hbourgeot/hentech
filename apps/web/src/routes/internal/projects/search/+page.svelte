0b5aa4e4e2cf2a9f4c45d237d5f1b1cfef058220
henrry <henrrybrgt@gmail.com>
2023-09-17 01:28:24 -0400

    adding more styles

--- apps/web/src/routes/internal/projects/search/+page.svelte
+++ apps/web/src/routes/internal/projects/search/+page.svelte
@@ -30,6 +30,35 @@
 
 <main class="w-full">
 	<h1 class="text-3xl font-bold text-center">Search Projects</h1>
+    <section class="grid grid-cols-3 gap-3 my-3 border-b p-3 pb-8 border-b-primary-400/30">
+        <label for="id" class="label">
+            <span>ID</span>
+            <input type="text" class="input (text)" name="id" id="id">
+        </label>
+        <label for="name" class="label">
+            <span>name</span>
+            <input type="text" class="input (text)" name="name" id="name">
+        </label>
+        <label for="comercialDesignation" class="label">
+            <span>Comercial Designation</span>
+            <input type="text" class="input (text)">
+        </label>
+        <label for="leader" class="label">
+            <span>Leader</span>
+            <input type="text" class="input" id="leader" name="leader">
+        </label>
+        <label for="status" class="label">
+            <span>Status</span>
+            <select id="status" name="status" required class="select rounded-full">
+				<option value="Active">Active</option>
+				<option value="Inactive">Inactive</option>
+				<option value="Abandoned">Abandoned</option>
+				<option value="Finished">Finished</option>
+				<option value="Pending">Pending</option>
+			</select>
+        </label>
+    </section>
+    <h2 class="text-2xl font-semibold">Projects List</h2>
 	<table class="table !my-4">
         <tr>
             {#each headers as header}
