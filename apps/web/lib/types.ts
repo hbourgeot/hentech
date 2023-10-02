b975f193e09d8921bf348ab8306c3d7380ac2c27
henrry <henrrybrgt@gmail.com>
2023-10-02 07:39:06 -0400

    adding interfaces

---
+++ apps/web/lib/types.ts
@@ -0,0 +1,35 @@
+export interface Employee {
+  address: string;
+  email: string;
+  employeeProjects: EmployeeProject[];
+  id: number;
+  lastName: string;
+  name: string;
+  password: string;
+  phoneNumber: string;
+  role: Role;
+}
+
+export interface EmployeeProject {
+  comercialDesignation: string;
+  id: number;
+  name: string;
+  status: string;
+  type: null;
+}
+
+export interface Role {
+  id: number;
+  role: string;
+}
+
+export interface Project {
+  comercialDesignation: string;
+  employees: Employee[];
+  id: number;
+  leader: Employee;
+  name: string;
+  status: string;
+  tasks: any[];
+  type: null | string;
+}
