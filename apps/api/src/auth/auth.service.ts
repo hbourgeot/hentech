3cefeb8bf388d7153131c3f08a1d72baa8b7f9e2
henrry <henrrybrgt@gmail.com>
2023-10-04 22:10:11 -0400

    fixing auth service

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Employee } from 'src/employee/entity/employee.entity';
 import { Repository } from 'typeorm';
@@ -7,21 +7,37 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectRepository(Employee) private repo: Repository<Employee>, private jwtService: JwtService) { }
-  
+  constructor(
+    @InjectRepository(Employee) private repo: Repository<Employee>,
+    private jwtService: JwtService,
+  ) {}
+
   async signIn(email: string, pass: string): Promise<any> {
-    const user = await this.repo.findOne({ where: { email: email }, relations: {role: true} });
+    const user = await this.repo.findOne({
+      where: { email: email },
+      relations: { role: true },
+    });
     if (!user) {
-      throw new NotFoundException();
+      return {
+        token: null,
+        ok: false,
+        status: 400,
+        message: 'Invalid credentials',
+      };
     }
 
     if (await bcrypt.compare(pass, user.password)) {
-      throw new UnauthorizedException();
+      return {
+        token: null,
+        ok: false,
+        status: 400,
+        message: 'Invalid credentials',
+      };
     }
 
-    const payload = {sub: user.id, employee: user}
+    const payload = { sub: user.id, employee: user };
     return {
-      token: await this.jwtService.signAsync(payload, {expiresIn: "30m"}),
+      token: await this.jwtService.signAsync(payload, { expiresIn: '30m' }),
       ok: true,
       status: 200,
     };
