e3972d297220b52af4e4d4faf44298429e3e9919
henrry <henrrybrgt@gmail.com>
2023-10-04 22:10:41 -0400

    fixing and testing

--- apps/web/pages/internal/index.tsx
+++ apps/web/pages/internal/index.tsx
@@ -1,21 +1,25 @@
-import { GetServerSideProps } from 'next';
+import { Nav } from "@/components/Nav";
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
 
-const internal = () => {
+function Internal() {
+  let enter = 0;
+  const router = useRouter();
+  if (typeof window !== "undefined" && enter === 0) {
+    enter++;
+  }
   return (
     <div>
+      <Nav />
       Enter
     </div>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-
-
   return {
-    props:{
+    props: {},
+  };
+};
 
-    }
-  }
-}
-
-export default internal
+export default Internal;
