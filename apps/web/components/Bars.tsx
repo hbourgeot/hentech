ba2ef31ebe81c2757e9ad8202410176564fc5e10
henrry <henrrybrgt@gmail.com>
2023-10-12 09:37:46 -0400

    fixing comps

--- apps/web/components/Bars.tsx
+++ apps/web/components/Bars.tsx
@@ -1,4 +1,5 @@
-import { PieChart, Pie, ResponsiveContainer } from "recharts";
+import { useState } from "react";
+import { PieChart, Pie, ResponsiveContainer, Sector } from "recharts";
 
 let data01 = [
   { name: "Group A", value: 400 },
@@ -22,46 +23,126 @@ let data02 = [
 
 interface Props {
   inner: Array<{ name: string; value: number }>;
-  outer: Array<{ name: string; value: number }>;
-  outerRadius: number;
   innerRadius: number;
   fillInner?: string;
-  fillOuter?: string;
+  className?: string;
 }
+
+const renderActiveShape = (props: any) => {
+  const RADIAN = Math.PI / 180;
+  const {
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    percent,
+    value,
+  } = props;
+  
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? "start" : "end";
+
+  return (
+    <g>
+      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+        {payload.name}
+      </text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+      <path
+        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+        stroke={fill}
+        fill="none"
+      />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        textAnchor={textAnchor}
+        fill="#333">{`${payload.name}`}</text>
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        dy={18}
+        textAnchor={textAnchor}
+        fill="#999">
+        {`(Rate ${(percent * 100).toFixed(2)}%`}
+      </text>
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey+20}
+        dy={18}
+        textAnchor={textAnchor}
+        fill="#999">
+        {`Value: ${value})`}
+      </text>
+    </g>
+  );
+};
 
 export function Bars({
   inner,
-  outer,
   fillInner = "#8884d8",
-  fillOuter = "#82ca9d",
-  outerRadius,
-  innerRadius
+  innerRadius,
+  className,
 }: Props) {
-  console.log(inner.length, outer.length)
-  if(!inner.length) inner = data01;
-  if(!outer.length) outer = data02;
+  if (!inner.length) inner = data01;
+
+  const [state, setState] = useState({ activeIndex: 0 });
+
+  const onPieEnter = (_: any, index: number) => {
+    setState({
+      activeIndex: index,
+    });
+  };
+
   return (
-    <ResponsiveContainer width="100%" height="100%">
-      <PieChart width={400} height={400}>
-        <Pie
-          data={data01}
-          dataKey="value"
-          cx="50%"
-          cy="50%"
-          outerRadius={innerRadius}
-          fill={fillInner}
-        />
-        <Pie
-          data={data02}
-          dataKey="value"
-          cx="50%"
-          cy="50%"
-          innerRadius={innerRadius+10}
-          outerRadius={outerRadius}
-          fill={fillOuter}
-          label
-        />
-      </PieChart>
-    </ResponsiveContainer>
+    <div className={className}>
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart width={400} height={400}>
+          <Pie
+            activeIndex={state.activeIndex}
+            activeShape={renderActiveShape}
+            onMouseEnter={onPieEnter}
+            data={inner}
+            dataKey="value"
+            cx="50%"
+            cy="50%"
+            legendType="cross"
+            innerRadius={innerRadius}
+            outerRadius={innerRadius+((innerRadius+20)/2)}
+            fill={fillInner}
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
