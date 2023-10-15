00eac77a6ecf109b26b75f90a736957a0e2b19d5
henrry <henrrybrgt@gmail.com>
2023-10-15 00:38:47 -0400

    starting search

---
+++ apps/web/pages/internal/projects/search/index.tsx
@@ -0,0 +1,103 @@
+import { Combobox } from '@/components/Combobox';
+import { Nav } from '@/components/Nav'
+import { SelectInput } from '@/components/Select';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input'
+import { client } from '@/lib/axios';
+import { Employee } from '@/lib/types';
+import { NextPage, GetServerSideProps } from 'next'
+import * as z from "zod"
+
+interface Props {
+  employees: {label: string, value: number}
+}
+
+const stateOptions = [
+  { label: "Active", value: "Active" },
+  { label: "Inactive", value: "Inactive" },
+  { label: "Pending", value: "Pending" },
+  { label: "Finished", value: "Finished" },
+  { label: "Abandoned", value: "Abandoned" },
+];
+
+function Index({employees}: Props) {
+  return (
+    <>
+      <Nav />
+      <section className="h-screenav">
+        <h2 className="text-3xl font-semibold text-center py-3">
+          Search projects
+        </h2>
+        <div className="mx-auto pb-16 pt-4 flex items-start justify-between">
+          <form className="max-w-[70vw] mx-auto flex flex-wrap w-full place-content-center">
+            <span className="w-1/3 p-4">
+              <label htmlFor="id">Project ID</label>
+              <Input name="id" />
+            </span>
+            <span className="w-1/3 p-4">
+              <label htmlFor="name">Project Name</label>
+              <Input name="name" />
+            </span>
+            <span className="w-1/3 p-4">
+              <label htmlFor="comercialDesignation">
+                Commercial Designation
+              </label>
+              <Input name="comercialDesignation" />
+            </span>
+            <span className="w-1/3 p-4">
+              <label htmlFor="status">Project Status</label>
+              <SelectInput
+                data={stateOptions}
+                label="Select a status"
+                placeholder=""
+                className="!w-full"
+              />
+            </span>
+            <span className="w-1/3 p-4 flex flex-col">
+              <label htmlFor="leader">Leader</label>
+              <Combobox
+                data={employees}
+                label="Search a leader"
+                placeholder="Leader..."
+                className="w-full"
+              />
+            </span>
+            <span className="w-1/3 p-4">
+              <label htmlFor="type">Project Type</label>
+              <Input name="type" />
+            </span>
+            <div className='my-4 flex flex-col md:flex-row gap-4'>
+              <Button variant={'secondary'}>Clear filters</Button>
+              <Button variant={'default'} >Search</Button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  try {
+    const { data }: { data: Employee[] } = await client.get("/api/employees");
+    console.log(data);
+
+    const employees = data.map((item) => ({
+      label: `${item.name} ${item.lastName}`,
+      value: item.id,
+    }));
+    return {
+      props: {
+        employees: employees,
+      },
+    };
+  } catch (e) {
+    return {
+      props: {
+        employees: [],
+      },
+    };
+  }
+}
+
+export default Index
