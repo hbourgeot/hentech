16b6af5e08a510034374bf47a943903925ed74ed
henrry <henrrybrgt@gmail.com>
2023-10-14 23:58:12 -0400

    adding toggle mode

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -1,23 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 export default function Document() {
   return (
     <Html lang="en">
